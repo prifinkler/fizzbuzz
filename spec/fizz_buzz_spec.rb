e503713bb8a4197945fdd8f0dfe9ce448b520811
Priscila Finkler <prifinkler@gmail.com>
2023-10-23 00:09:49 +0100

    Initial commit

---
+++ spec/fizz_buzz_spec.rb
@@ -0,0 +1,8 @@
+require "fizz_buzz"
+
+RSpec.describe FizzBuzz do
+  it "outputs the numbers from 1 to 100"
+  it "outputs Fizz for multiples of 3"
+  it "outputs Buzz for multiples of 5"
+  it "outputs FizzBuzz for multiples of 3 and 5"
+end
