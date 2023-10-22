f9d0fa59ff7b22854aa656f5df06b7d00b110087
Priscila Finkler <prifinkler@gmail.com>
2023-10-23 00:51:15 +0100

    Solution and TDD

--- lib/fizz_buzz.rb
+++ lib/fizz_buzz.rb
@@ -1,2 +1,15 @@
-module FizzBuzz
+class FizzBuzz
+  def self.outputs
+    (1..100).map do |n|
+      if n % 3 == 0 && n % 5 == 0
+        'FizzBuzz'
+      elsif n % 3 == 0
+        'Fizz'
+      elsif n % 5 == 0
+        'Buzz'
+      else
+        n.to_s
+      end
+    end
+  end
 end
--- spec/fizz_buzz_spec.rb
+++ spec/fizz_buzz_spec.rb
@@ -1,8 +1,28 @@
 require "fizz_buzz"
 
 RSpec.describe FizzBuzz do
-  it "outputs the numbers from 1 to 100"
-  it "outputs Fizz for multiples of 3"
-  it "outputs Buzz for multiples of 5"
-  it "outputs FizzBuzz for multiples of 3 and 5"
+  subject(:outputs) { described_class.outputs }
+
+  it "starts at 1" do
+    expect(outputs.first).to eq('1')
+  end
+
+  it "has 100 outputs" do
+    expect(outputs.size).to eq(100)
+  end
+
+  it "outputs Fizz for multiples of 3" do
+    expect(outputs[2]).to eq('Fizz')
+    expect(outputs[5]).to eq('Fizz')
+  end
+
+  it "outputs Buzz for multiples of 5" do
+    expect(outputs[4]).to eq('Buzz')
+    expect(outputs[9]).to eq('Buzz')
+  end
+
+  it "outputs FizzBuzz for multiples of 3 and 5" do
+    expect(outputs[14]).to eq('FizzBuzz')
+    expect(outputs[29]).to eq('FizzBuzz')
+  end
 end
